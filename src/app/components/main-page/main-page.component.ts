d1f5e721884a624405842ba4c46cc46bbad44635
Metwet <tselm.matv@gmail.com>
2023-10-29 00:18:55 +0300

    created sorting books

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -17,4 +17,39 @@ export class MainPageComponent {
       this.books = data;
     });
   }
+
+  isTitleSortedAscending: boolean = true;
+  isPriceSortedAscending: boolean = true;
+
+  sortBooksByTitle(){
+    this.isTitleSortedAscending = !this.isTitleSortedAscending;
+    this.books.sort((a, b)=>{
+      const titleA = a.title.toLowerCase();
+      const titleB = b.title.toLowerCase();
+      const direction = this.isTitleSortedAscending ? -1 : 1;
+      if (titleA < titleB) {
+        return -1 * direction;
+      }
+      if (titleA > titleB) {
+        return 1 * direction;
+      }
+      return 0;
+    })
+  }
+
+  sortBooksByPrice(){
+    this.isPriceSortedAscending = !this.isPriceSortedAscending;
+    this.books.sort((a,b)=>{
+      const priceA = parseFloat(a.price.replace('$', ''));
+      const priceB = parseFloat(b.price.replace('$', ''));
+      const direction = this.isPriceSortedAscending ? -1 : 1;
+      if(priceA < priceB){
+        return -1 * direction;
+      }
+      if(priceA > priceB){
+        return 1 * direction;
+      }
+      return 0;
+    })
+  }
 }
